12da9cfd7906b25a4a21a8a308c8b4608f6e3700
Andreina Guerra <141190940+Gealuque@users.noreply.github.com>
2025-02-08 09:45:02 -0300

    Cambios alertas y hide alerts

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,6 @@
 // Hito_2
 import './Register_Login.css'
+import Swal from 'sweetalert2'
 import { useState } from 'react'
 
 const Login = () => {
@@ -8,6 +9,7 @@ const Login = () => {
     email: '',
     clave: ''
   })
+  const [mostrarClave, setMostrarClave] = useState(false)
   // Controlamos los eventos de cambio
   const handleCambio = (evento) => {
     setLogin({ ...login, [evento.target.name]: evento.target.value })
@@ -22,22 +24,39 @@ const Login = () => {
     const { email, clave } = login
     // Condiciones
     if (!email.trim() || !clave.trim()) {
-      alert('Todos los campos deben estar llenos')
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Todos los campos deben ser completados!'
+      })
       return
     }
 
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
     if (!emailRegex.test(email)) {
-      alert('Ingrese un mail válido')
+      Swal.fire({
+        icon: 'error',
+        text: 'Ingrese un mail válido'
+      })
+      setLogin({ email: '', clave: '' })
       return
     }
 
     if (clave.length < 6 || clave.length > 14) {
-      alert('La clave deben contener entre 6 y 14 carateres')
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'La clave deben contener entre 6 y 14 carateres'
+      })
+      setLogin({ clave: '' })
       return
     }
 
-    alert('Login Éxitoso!')
+    Swal.fire({
+      title: 'Login éxitoso!',
+      icon: 'success',
+      draggable: true
+    })
     setLogin({ email: '', clave: '' })
   }
 
@@ -57,12 +76,15 @@ const Login = () => {
           </div>
           <div className='box_input'>
             <label>Contraseña</label>
-            <input
-              type='password'
-              name='clave'
-              value={login.clave}
-              onChange={handleCambio}
-            />
+            <div>
+              <input
+                type={mostrarClave ? 'text' : 'password'}
+                name='clave'
+                value={login.clave}
+                onChange={handleCambio}
+              />
+              <button type='button' onClick={() => setMostrarClave(!mostrarClave)} className='toggle-password'> {mostrarClave ? '👀' : '🫣'} </button>
+            </div>
           </div>
         </div>
         <button type='submit' className='btn btn-primary'>Logearse</button>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,6 @@
 // Hito_2
 import './Register_Login.css'
+import Swal from 'sweetalert2'
 import { useState } from 'react'
 
 const RegisterPage = () => {
@@ -9,6 +10,8 @@ const RegisterPage = () => {
     clave: '',
     confirmar: ''
   })
+  const [mostrarClave, setMostrarClave] = useState(false)
+  const [mostrarConfirmar, setMostrarConfirmar] = useState(false)
   // Controlamos los eventos de cambio
   const handleCambio = (evento) => {
     setRegistro({ ...registro, [evento.target.name]: evento.target.value })
@@ -23,27 +26,49 @@ const RegisterPage = () => {
     const { email, clave, confirmar } = registro
     // Condiciones
     if (!email.trim() || !clave.trim() || !confirmar.trim()) {
-      alert('Todos los campos deben estar llenos')
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Todos los campos deben ser completados!'
+      })
+      setRegistro({ email: '', clave: '', confirmar: '' })
       return
     }
 
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
     if (!emailRegex.test(email)) {
-      alert('Ingrese un mail válido')
+      Swal.fire({
+        icon: 'error',
+        text: 'Ingrese un mail válido'
+      })
+      setRegistro({ email: '', clave: '', confirmar: '' })
       return
     }
 
     if (clave.length < 6 || clave.length > 14) {
-      alert('La clave deben contener entre 6 y 14 carateres')
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'La clave deben contener entre 6 y 14 carateres'
+      })
+      setRegistro({ email: '', clave: '', confirmar: '' })
       return
     }
 
     if (clave !== confirmar) {
-      alert('Las claves deben ser iguales')
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Las claves deben ser iguales'
+      })
+      setRegistro({ email: '', clave: '', confirmar: '' })
       return
     }
-
-    alert('Registro éxitoso!')
+    Swal.fire({
+      title: 'Registro éxitoso!',
+      icon: 'success',
+      draggable: true
+    })
     setRegistro({ email: '', clave: '', confirmar: '' })
   }
 
@@ -63,21 +88,27 @@ const RegisterPage = () => {
           </div>
           <div className='box_input'>
             <label>Contraseña</label>
-            <input
-              type='password'
-              name='clave'
-              value={registro.clave}
-              onChange={handleCambio}
-            />
+            <div>
+              <input
+                type={mostrarClave ? 'text' : 'password'}
+                name='clave'
+                value={registro.clave}
+                onChange={handleCambio}
+              />
+              <button type='button' onClick={() => setMostrarClave(!mostrarClave)} className='toggle-password'> {mostrarClave ? '👀' : '🫣'} </button>
+            </div>
           </div>
           <div className='box_input'>
             <label>Confirmar Contraseña</label>
-            <input
-              type='password'
-              name='confirmar'
-              value={registro.confirmar}
-              onChange={handleCambio}
-            />
+            <div>
+              <input
+                type={mostrarConfirmar ? 'text' : 'password'}
+                name='confirmar'
+                value={registro.confirmar}
+                onChange={handleCambio}
+              />
+              <button type='button' onClick={() => setMostrarConfirmar(!mostrarConfirmar)} className='toggle-password'> {mostrarConfirmar ? '👀' : '🫣'} </button>
+            </div>
           </div>
         </div>
         <button type='submit' className='btn btn-primary'>Enviar</button>
