51f9d8caf1339a28e202c486c1bcc05e4307eda8
Andreina Guerra <141190940+Gealuque@users.noreply.github.com>
2025-02-15 15:29:40 -0300

    Totale y funcionalidades

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import Navbar from './components/Navbar'
-import Home from './components/Home'
+/* import Home from './components/Home' */
 import Footer from './components/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 /* import RegisterPage from './components/Register' */
 /* import Login from './components/Login' */
+import Cart from './components/Cart'
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Home />
+      {/* <Home /> */}
       {/* <RegisterPage /> */}
       {/* <Login /> */}
+      <Cart />
       <Footer />
     </>
   )
